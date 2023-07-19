8c562e3b179bb5eb5522b6785bbffa441b1dfadb
beruangdev <beruangdev@gmail.com>
2023-07-20 05:34:21 +0700

    'update'

--- header.php
+++ header.php
@@ -35,13 +35,33 @@ $is_desktop = !$is_mobile && !$is_tablet;
 ?>
 
 <!DOCTYPE html>
-<html lang="en" class="<?= $_COOKIE['color-theme']?>">
-
+<html <?php language_attributes(); ?> class="<?= $_COOKIE['color-theme']?>">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    
+    <!-- Judul Halaman -->
+    <title><?php wp_title('|', true, 'right'); ?><?php bloginfo('name'); ?></title>
+    
+    <!-- Deskripsi Situs -->
+    <meta name="description" content="<?php bloginfo('description'); ?>">
+    
+    <!-- Meta Tag untuk Penelusuran -->
+    <meta name="robots" content="index, follow">
+    <meta name="googlebot" content="index, follow">
+    
+    <!-- Canonical URL -->
+    <?php if (is_singular()) : ?>
+        <link rel="canonical" href="<?php the_permalink(); ?>">
+    <?php endif; ?>
+    
+    <!-- Sitemap XML -->
+    <link rel="sitemap" type="application/xml" title="Sitemap" href="<?php echo esc_url(home_url('/sitemap_index.xml')); ?>">
+    
+    <!-- Favicon -->
+    <link rel="icon" href="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/favicon/favicon.ico" type="image/x-icon">
+    
     <?php wp_head() ?>
 </head>
 
