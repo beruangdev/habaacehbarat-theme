077735d0b476b0a04106c305d31a286bd0ddcbb6
beruangdev <beruangdev@gmail.com>
2023-07-20 05:37:15 +0700

    'update'

--- header.php
+++ header.php
@@ -35,39 +35,54 @@ $is_desktop = !$is_mobile && !$is_tablet;
 ?>
 
 <!DOCTYPE html>
-<html <?php language_attributes(); ?> class="<?= $_COOKIE['color-theme']?>">
+<html <?php language_attributes(); ?> class="<?= $_COOKIE['color-theme'] ?>">
+
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
     <!-- Judul Halaman -->
     <title><?php wp_title('|', true, 'right'); ?><?php bloginfo('name'); ?></title>
-    
+
     <!-- Deskripsi Situs -->
     <meta name="description" content="<?php bloginfo('description'); ?>">
-    
+
     <!-- Meta Tag untuk Penelusuran -->
     <meta name="robots" content="index, follow">
     <meta name="googlebot" content="index, follow">
-    
+
     <!-- Canonical URL -->
     <?php if (is_singular()) : ?>
         <link rel="canonical" href="<?php the_permalink(); ?>">
     <?php endif; ?>
-    
+
     <!-- Sitemap XML -->
     <link rel="sitemap" type="application/xml" title="Sitemap" href="<?php echo esc_url(home_url('/sitemap_index.xml')); ?>">
-    
+
     <!-- Favicon -->
     <link rel="icon" href="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/favicon/favicon.ico" type="image/x-icon">
-    
+
+    <!-- Apple Touch Icon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/favicon/apple-touch-icon.png">
+
+    <!-- Android Chrome Icons -->
+    <link rel="icon" type="image/png" sizes="192x192" href="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/favicon/android-chrome-192x192.png">
+    <link rel="icon" type="image/png" sizes="512x512" href="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/favicon/android-chrome-512x512.png">
+
+    <!-- Other Favicons -->
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/favicon/favicon-16x16.png">
+
+    <!-- Web Manifest -->
+    <link rel="manifest" href="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/favicon/site.webmanifest">
+
     <?php wp_head() ?>
 </head>
 
 <body <?php body_class('theme-habaacehbarat') ?>>
     <?php wp_body_open(); ?>
 
-    <?= get_template_part('template-parts/component/navbar') ?> 
+    <?= get_template_part('template-parts/component/navbar') ?>
 
     <main class="flex-grow w-full md:mt-8">
